b1f21ab7d783cf5adbe69c802ca971f81e655b62
takeahot <mirtrudmaynow@gmail.com>
2020-11-15 13:15:53 +0300

    'add diff'

--- app.js
+++ app.js
@@ -1,62 +1,67 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error',{title: 'error'});
+  res.render("error", { title: "error" });
 });
-
 
 module.exports = app;
 
-const PizZip = require('pizzip');
-const Docxtemplater = require('docxtemplater');
+const PizZip = require("pizzip");
+const Docxtemplater = require("docxtemplater");
 
-const fs = require('fs');
+const fs = require("fs");
 // const path = require('path');
 
-let content = fs
-  .readFileSync(path.resolve(__dirname,'test.docx'),'binary');
-
-let zip = new PizZip(content);
-let doc;
-try {
+function getTextFromDoc (file_name) {
+  //read file into var binary format
+  let content = fs.readFileSync(path.resolve(__dirname, file_name), "binary");
+  //unzip content
+  let zip = new PizZip(content);
+  //convert open office xml to docxtemplate object 
+  let doc;
+  try {
     doc = new Docxtemplater(zip);
-} catch(error) {
+  } catch (error) {
     // Catch compilation errors (errors caused by the compilation of the template : misplaced tags)
     errorHandler(error);
+  }
+  //return text from docxtemplate object
+  return doc.getFullText();
 }
-var text = doc.getFullText()
-console.log(text,'text');
+
+let t1 = getTextFromDoc ('text.docx');
+let t2 = getTextFromDoc ('text2.docx');
