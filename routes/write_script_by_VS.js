eeaf342237b4edb5a16411dd2b3ab4784ac6280c
takeahot <mirtrudmaynow@gmail.com>
2020-12-29 18:56:51 +0300

    'add example from ejs'

---
+++ routes/write_script_by_VS.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var {Auth} = require('../src/use_VScode_for_script_building/main.js')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  iqtech_mv = {
+    u : 'https://iqtechnology.casepro.pro/',
+    l : 'a.arestov@pravo.ru',
+    p : 'PrAn!000'
+  };
+
+  let runIq = new Auth(iqtech_mv.u,iqtech_mv.l,iqtech_mv.p);
+  runIq.getToken()
+    .then(token => {
+      console.log(runIq);
+    }
+  )
+
+
+});
+
+module.exports = router;
