d0bcc3cb5180752540dda17e01166fefd695f982
takeahot <mirtrudmaynow@gmail.com>
2021-01-11 18:59:53 +0300

    - create site which get token and chow page with message

--- routes/write_script_by_VS.js
+++ routes/write_script_by_VS.js
@@ -1,22 +1,39 @@
 var express = require('express');
 var router = express.Router();
-var {Auth} = require('../src/use_VScode_for_script_building/main.js')
+var VSSB = require('../src/use_VScode_for_script_building/main.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   iqtech_mv = {
     u : 'https://iqtechnology.casepro.pro/',
-    l : 'a.arestov@pravo.ru',
-    p : 'PrAn!000'
+    l : 'a.arestov@pravo.tech',
+    p : 'PrAn!M67'
   };
 
-  let runIq = new Auth(iqtech_mv.u,iqtech_mv.l,iqtech_mv.p);
+  console.log(VSSB.auth);
+
+  let runIq = new VSSB.auth(iqtech_mv.u,iqtech_mv.l,iqtech_mv.p);
   runIq.getToken()
     .then(token => {
-      console.log(runIq);
+      {
+  "Id": "string",
+  "Name": "string",
+  "Description": "string",
+  "IsActive": true,
+  "Type": {
+    "SysName": "string",
+    "Id": "string",
+    "Name": "string"
+  },
+  "FolderId": "string"
+}
+      console.log('the token is ' + token);
+
+      res.locals.header = "Результаты авторизации";
+      res.locals.message = runIq
+      res.render('index',res.locals);
     }
   )
-
 
 });
 
