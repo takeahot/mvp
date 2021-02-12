2c909cbcfa791a9a1bd9bef20f2644ba89ca3ef3
takeahot <mirtrudmaynow@gmail.com>
2021-02-12 15:10:14 +0300

    - delete .env

--- main.js
+++ main.js
@@ -9,8 +9,38 @@ let fD1toD1 = async () => {
   let template2 = new VSSB.Template(auth2)
   let result = await template2.setTemplateFromData(Data);
 }
-fD1toD1()
+// fD1toD1()
       // res.locals.header = "Результаты авторизации";
       // res.locals.message = runIq
       // res.render('index',res.locals);
 
+//get list of bots from form.one
+const fetch = require('node-fetch');
+listOfBots = async () => {
+  let context = {};
+  obj = {
+      url: 'https://aem.form.one/web/user/login',
+      method: 'POST',
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+          "email":"a.arestov@pravo.tech",
+          "password":"PrAnAM67"
+          })
+  }
+  context.login = await fetch(obj.url, {method: obj.method, headers: obj.headers, body: obj.body});
+  console.log(await context.login,'cookie');
+  let cookie = context.login.headers.get('set-cookie');
+  obj = {
+      url: 'https://aem.form.one/web/template/1/bots?page=1',
+      method: 'GET',
+      headers: {
+        "cookie" : cookie
+      }
+  }
+  context.list = await fetch(obj.url,{method: obj.method, headers: obj.headers});
+  console.log((await context.list.json()).items.reduce((acc,val) => acc + val.title + '\r\n',""),'list');
+  
+}
+listOfBots()
