dd247b39142b8af3e839622f3d1b74de5f9166be
takeahot <mirtrudmaynow@gmail.com>
2021-03-02 00:35:24 +0300

    -delete fD1toD1 from main and create separate file

--- fromD1toD1.js
+++ fromD1toD1.js
@@ -4,7 +4,7 @@ var VSSB = require('./lib/main.js')
 let fD1toD1 = async () => {
   let auth = new VSSB.DocAuth(process.env.DURL,process.env.DLOGIN,process.env.DPASSWORD);
   let template = new VSSB.Template(auth)
-  Data = await template.getTemplateById("9135fefa-297e-11eb-b751-027b586fde61");
+  Data = await template.getTemplateById("4673c098-75fd-11eb-9113-027b580c882d");
   let auth2 = new VSSB.DocAuth(process.env.DURL2,process.env.DLOGIN2,process.env.DPASSWORD2)
   let template2 = new VSSB.Template(auth2)
   let result = await template2.setTemplateFromData(Data);
